6a5291205f92e6953bc289f613c0935b2c31a646
Mineadark <mineadark@gmail.com>
2024-11-07 01:28:07 +0100

    feat: if data missing, put N/A instead of error code

--- mainro.py
+++ mainro.py
@@ -5,7 +5,6 @@ import csv
 import os
 import re
 
-
 def scrape_autovit(max_page):
     base_url = "https://www.autovit.ro/autoturisme/second"
     params = {"search[order]": "filter_float_price:asc"}
@@ -14,7 +13,7 @@ def scrape_autovit(max_page):
     }
     data = []
 
-    for page in range(1, max_page + 1):
+    for page in range(151, max_page + 1):
         if page == 1:
             url = f"{base_url}?{requests.compat.urlencode(params)}"
         else:
@@ -33,7 +32,6 @@ def scrape_autovit(max_page):
                 details_div = item.find('dl', class_='ooa-1uwk9ii epwfahw11')
                 subheader = item.find('p', class_='epwfahw10 ooa-1tku07r er34gjf0')
 
-
                 year = "N/A"
                 km = "N/A"
                 fuel_type = "N/A"
@@ -45,37 +43,32 @@ def scrape_autovit(max_page):
 
                     details = details_text.split(' ')
 
-                    #
-
-                    km = details[0] + details[1]
-                    fuel_type = details[2]
-                    year = details[3]
+                    if len(details) == 4:
+                        km = details[0] + details[1]
+                        fuel_type = details[2]
+                        year = details[3]
+                    else:
+                        print(f"Missing data: {details_text}")
 
                 power = item.find('p', class_='epwfahw10').text.strip() if item.find('p', class_='epwfahw10') else "N/A"
-                price = item.find('div', class_='ooa-2p9dfw').text.strip() if item.find('div',
-                                                                                        class_='ooa-2p9dfw') else "N/A"
-                link = item.find('h1', class_='epwfahw9').find('a')['href'] if item.find('h1',
-                                                                                         class_='epwfahw9') and item.find(
-                    'h1', class_='epwfahw9').find('a') else "N/A"
-
+                price = item.find('div', class_='ooa-2p9dfw').text.strip() if item.find('div', class_='ooa-2p9dfw') else "N/A"
+                link = item.find('h1', class_='epwfahw9').find('a')['href'] if item.find('h1', class_='epwfahw9') and item.find('h1', class_='epwfahw9').find('a') else "N/A"
 
                 if subheader:
                     text = subheader.text.strip()
                     texts = text.split('•')
-                    power = texts[1].replace(' ','').replace('CP', '')
-
-
+                    if len(texts) > 1:
+                        power = texts[1].replace(' ','').replace('CP', '')
 
                 if title != "N/A" and price != "N/A":  # Only add non-empty entries
                     data.append([title, year, km, power, fuel_type, price, link])
 
             print(f"Scraped page {page}")
 
-            if page % 5 == 0:
+            if page % 50 == 0:
                 save_to_csv(data, f"autovit_data_page_{page}.csv")
                 data = []
             time.sleep(2)
-
         except requests.RequestException as e:
             print(f"An error occurred while scraping page {page}: {e}")
 
@@ -83,7 +76,6 @@ def scrape_autovit(max_page):
         save_to_csv(data, f"autovit_data_final.csv")
 
     print("Scraping completed")
-
 
 def save_to_csv(data, filename):
     headers = ['Title', 'Year', 'Km', 'Power', 'Fuel Type', 'Price', 'Link']
@@ -97,7 +89,6 @@ def save_to_csv(data, filename):
         writer.writerows(data)
 
     print(f"Data saved to {filepath}")
-
 
 # Run the scraper
 scrape_autovit(1230)
